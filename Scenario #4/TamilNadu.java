b3939ae7e3dd7a0afa619b85dd4f3704a7e9c609
meena-sundaram <44741122+meena-sundaram@users.noreply.github.com>
2019-10-13 21:15:32 +0530

    Add files via upload
    Expected Understanding: Abstraction, Inheritance, Dynamic Binding, Polymorphism (Overriding), Constructor Overloading
    1) Create an abstract class called 'India' 
    - Have below abstract methods 
      - void speakLanguage()
      - void eat()
      - void dress()
    - Have static variable String capital = "New Delhi" 
    - Have below Constructor 
    public India(String primeMinister)
    {
    System.out.println("our Prime Minister is" + primeMinister); 
    }
    2) Create an abstract class called 'SouthIndia' 
    - Make this class as sub class of 'India' 
    - Add below non abstract methods 
      - void cultivate()
      - Print 'Rice and Wheat cultivation' inside this method
      - void livingStyle() 
      - Print 'Average development' inside this method
    3) Create a class called 'TamilNadu' with main method as sub class of 'South India'. 
    - Add unimplemented methods 
    - Provide your own definitions wherever necessary.  
    - Have static variable String capital = "Chennai" 
    - Add below non abstract methods 
      - void cultivate() 
      - Print 'Rice and Sugar cane cultivation' inside this method
      - void livingStyle() 
      - Print 'Above Average development' inside this method
    - Using class name "India" - access variable 'capital' and print the value
    - Using class name "TamilNadu" - access variable 'capital' and print the value.  
    - Create instance for "SouthIndia" as below
    SouthIndia si = new TamilNadu()
    - Observe which methods and variables can be accessed using 'si' and note down.

---
+++ Scenario #4/TamilNadu.java
@@ -0,0 +1,55 @@
+package basic;
+
+abstract class India {                                                        //abstract class india
+	
+	abstract void speakLanguage();
+	abstract void eat();
+	abstract void dress();
+	static String capital = "New Delhi";
+
+	public India(String primeMinister) {                                     //parameterized constructor
+		System.out.println("our Prime Minister is " + primeMinister);
+	}
+}
+abstract class SouthIndia extends India{                                   //inherits india class
+	
+	void cultivate(){
+		System.out.println("Rice and Wheat cultivation"); 
+	}
+	void livingStyle() {
+		System.out.println("Average development");
+	}
+}
+public class TamilNadu extends SouthIndia {
+
+	static String capital = "Chennai";                                      //static variable
+
+	void speakLanguage() {                                                 //overriding methods
+		
+	}
+
+	void eat() {
+	
+	}
+
+	void dress() {
+		// TODO Auto-generated method stub
+		
+	}
+	 void cultivate() {
+		 System.out.println("Rice and Sugar cane is major cultivation in Tamilnadu");
+	 }
+	 void livingStyle() {
+		 System.out.println("Above Average development in Tamilnadu");
+	 }
+	public static void main(String[] args) {
+		System.out.println("India capital is "+India.capital);
+		System.out.println("TamilNadu capital is "+TamilNadu.capital);
+		SouthIndia si = new TamilNadu("Modi");                         //dynamic binding
+		si.cultivate();
+		si.livingStyle();
+
+	}
+
+
+}
