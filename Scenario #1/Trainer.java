35071f96ce9215cd45a3a610fff7138a7f26cc46
meena-sundaram <44741122+meena-sundaram@users.noreply.github.com>
2019-10-13 21:03:14 +0530

    Add files via upload
    Expected Understanding: Access Modifiers, Single Inheritance, getter methods, Constructor Overloading
    1) Create a Class named "Trainer".  
     - Have default instance variables String dept, institute
     - Have private instance variable int salary 
     - Assign 10000 as value for salary. 
     - Create getter method for salary.  
     - Assign values - "Java", "Payilagam" to them 
     - Have instance method training() with void as return data type
     - Add a print statement inside training() method 
     - Have instance named as 'trainerKumar' and pass "CSE", "payilagam" as arguments to it.  
     - Handle above line with matching Constructor.  
     
    2) Create a sub class "SQLTrainer" under "Trainer". 
     - Have main method in it. 
     - Create instance ram for this class 
     - Handle with proper super class constructor 
     - Access parent class instance variables
     - Call parent class instance method training() 
     - Access salary using getter method in parent class

---
+++ Scenario #1/Trainer.java
@@ -0,0 +1,29 @@
+package basic;
+
+public class Trainer {
+
+	String dept, institute;
+	private int salary = 1000;
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public Trainer() {
+		
+	}
+
+	private void training() {
+		// String name="trainerkumar";
+		System.out.println();
+
+	}
+
+}
+
+class SQLTrainer extends Trainer {
+	public static void main(String[] args) {
+		
+
+	}
+}
