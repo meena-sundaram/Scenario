6695a8fbed57f13867829656eca89b96d161af27
meena-sundaram <44741122+meena-sundaram@users.noreply.github.com>
2019-10-16 21:20:50 +0530

    completed

--- Scenario #1/Trainer.java
+++ Scenario #1/Trainer.java
@@ -1,29 +1,43 @@
 package basic;
 
-public class Trainer {
+class Trainer {
+	String dept = "CSE", institute = "Payilagam";
+	private int salary = 10000;                               //private data Access modifiers
 
-	String dept, institute;
-	private int salary = 1000;
-
-	public int getSalary() {
+	public int getSalary() {                                  //Encapsulation concepts in order to get the private data in a class getter() is used
 		return salary;
 	}
 
-	public Trainer() {
-		
+	public Trainer(String dept, String institute) {
+		this.dept = dept;
+		this.institute = institute;
 	}
 
-	private void training() {
-		// String name="trainerkumar";
-		System.out.println();
-
+	public void training() {
+		System.out.println("Department:" + dept + "\nInstitute:" + institute);
 	}
-
 }
 
-class SQLTrainer extends Trainer {
-	public static void main(String[] args) {
-		
+class SQLTrainer extends Trainer {                              //single inheritance
+	private int salary = 15000;
+	public int getSalary() {                                    //Encapsulation concepts in order to get the private data in a class getter() is used
+		return salary;                            
+	}
 
+	public SQLTrainer(String dept, String institute) {         //constructor 
+		super(dept, institute);
+		this.dept = dept;
+		this.institute = institute;
+	}
+
+	public static void main(String[] args) {
+		Trainer trainerkumar = new Trainer("Java", "Payilagam");
+		System.out.println("Trainer:Kumar");
+		trainerkumar.training();
+		System.out.println("Salary:" + trainerkumar.getSalary());
+		SQLTrainer ram = new SQLTrainer("SQL", "Payilagam");
+		System.out.println("Trainer:Ram");
+		ram.training();
+		System.out.println("Salary:" + ram.getSalary());
 	}
 }
